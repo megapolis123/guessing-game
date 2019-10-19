22a882300e08504a4550a3c0507f1172af95f8e9
megapolis123 <al.averin123@gmail.com>
2019-10-19 16:30:03 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentValue = Math.round((this.min + this.max) / 2);
+        return this.currentValue;
     }
 
     lower() {
-
+        this.max = this.currentValue;
     }
 
     greater() {
-
+        this.min = this.currentValue;
     }
 }
 
